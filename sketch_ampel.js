f6db521460a866ff90908acad24f044cd758ec17
nils-e13 <nils@sup2o.com>
2021-10-27 09:18:00 +0200

    creation of index.html

---
+++ sketch_ampel.js
@@ -0,0 +1,28 @@
+function setup() {
+    createCanvas(200, 200);
+}
+
+function draw() {
+    rectMode(CENTER);
+    background(0);
+    fill('gray');
+    rect(100, 100, 50, 100);
+    stroke(0); //creates a black circle for each light
+    strokeWeight(2);
+    ellipse(100, 70, 21);
+    ellipse(100, 100, 21);
+    ellipse(100, 130, 21);
+
+        if (mouseY > 60 && mouseY < 90) { //starts at circle radius (70-10=60)
+            fill('red');
+            ellipse(100, 70, 20, 20);
+        }
+        if (mouseY > 90 && mouseY < 120) {
+            fill('yellow');
+            ellipse(100, 100, 20, 20);
+        }
+        if (mouseY > 120 && mouseY < 140) {
+            fill('green');
+            ellipse(100, 130, 20, 20);
+        }
+}
