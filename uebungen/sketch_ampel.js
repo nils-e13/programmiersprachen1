3c46570925b7cb543f95ba4234eb254257730e86
nils-e13 <nils@sup2o.com>
2021-11-01 14:13:56 +0100

    added X-Axis as conditional statement to change color

--- uebungen/sketch_ampel.js
+++ uebungen/sketch_ampel.js
@@ -13,15 +13,15 @@ function draw() {
     ellipse(100, 100, 21);
     ellipse(100, 130, 21);
 
-        if (mouseY > 60 && mouseY < 90) { //starts at circle radius (70-10=60)
+        if (mouseY > 60 && mouseY < 90 && mouseX > 90 && mouseX < 110) { //fills circle with color when mouse is hovered above circle
             fill('red');
             ellipse(100, 70, 20, 20);
         }
-        if (mouseY > 90 && mouseY < 120) {
+        if (mouseY > 90 && mouseY < 120 && mouseX > 90 && mouseX < 110) {
             fill('yellow');
             ellipse(100, 100, 20, 20);
         }
-        if (mouseY > 120 && mouseY < 140) {
+        if (mouseY > 120 && mouseY < 140 && mouseX > 90 && mouseX < 110) {
             fill('green');
             ellipse(100, 130, 20, 20);
         }
