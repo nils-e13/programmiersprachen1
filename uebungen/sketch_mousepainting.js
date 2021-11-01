7ca7bade47531fc4710462164c28cc739c8b7d9b
nils-e13 <nils@sup2o.com>
2021-11-01 14:05:02 +0100

    rework of code

--- uebungen/sketch_mousepainting.js
+++ uebungen/sketch_mousepainting.js
@@ -4,20 +4,9 @@ function setup() {
 }
 
 function draw() {
-    rectMode(CENTER);
-    fill ('red');
-    rect(100, 100, 80, 50);
-
-    fill('blue');
-    rect(250, 100, 80, 50);
-
-    fill('green');
-    rect(400, 100, 80, 50);
-
-    fill('white');
-    rect(250, 400, 80, 50);
-
-
+  if (mouseX < 60) {
+    fill('white')
+  }
   if (mouseX > 60) {
     fill('red');
   }
@@ -36,8 +25,35 @@ function draw() {
   if (mouseX > 440) {
       fill('white');
   }
-
+  
+  if (mouseIsPressed && mouseY > 360 && mouseY < 440 && mouseX > 225 && mouseX < 275) {
+    background(0);
+  }
   if (mouseIsPressed && mouseY > 125) {
     circle(mouseX, mouseY, 25);
   }
+
+    rectMode(CENTER);
+    fill ('red');
+    rect(100, 100, 80, 50);
+
+    fill('blue');
+    rect(250, 100, 80, 50);
+
+    fill('green');
+    rect(400, 100, 80, 50);
+
+    fill('white');
+    rect(250, 400, 80, 50);
+
+    fill(255);
+    textSize(15);
+    textFont('Helvetica');
+    text('Red', 85, 105);
+    text('Blue', 235, 105);
+    text('Green',380, 105);
+    fill(0);
+    textSize(15);
+    textFont('Helvetica');
+    text('Reset',230, 405);
 }
