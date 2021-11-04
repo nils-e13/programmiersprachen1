3321b3ad8a6e5acf0c858701adfb685548a8c606
nils-e13 <nils@sup2o.com>
2021-11-04 14:39:08 +0100

    addition of for loop

--- uebungen/sketch_loop_circles.js
+++ uebungen/sketch_loop_circles.js
@@ -4,6 +4,9 @@ const offset = 15;
 let xCoordinate = diameter;
 let yCoordinate = diameter;
 
+let i;
+let j;
+
 function setup() {
     createCanvas(650, 650);
     background(0);
@@ -13,7 +16,7 @@ function draw() {
     fill('white')
     while(yCoordinate < height) {
         circle(xCoordinate, yCoordinate, diameter);
-    
+        
         while(xCoordinate < width) {
             circle(xCoordinate, yCoordinate, diameter);
             xCoordinate = xCoordinate + diameter + offset;
@@ -22,4 +25,15 @@ function draw() {
     xCoordinate = diameter;
     yCoordinate = yCoordinate + diameter + offset;
 }
+
+//for loop
+/*
+function draw() {
+    fill('white')
+    for(i = diameter; i < width; i =i + diameter + offset) {
+        for(j = diameter; j < height; j = j + diameter + offset) {
+            circle(i, j, diameter)
+        }
+    }
+}*/
 }
