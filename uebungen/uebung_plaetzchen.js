5eaf81adf85d4517fae4c338e4ace820ac347ad8
nils-e13 <nils@sup2o.com>
2021-12-01 10:32:14 +0100

    plaetzchen updates

--- uebungen/uebung_plaetzchen.js
+++ uebungen/uebung_plaetzchen.js
@@ -6,10 +6,10 @@ const minCoordinate = plateCoordinates - 0.5 * plateDiameter + diameter
 const maxCoordinate = plateCoordinates + 0.5 * plateDiameter - diameter
 let max = maxCoordinate;
 let min = minCoordinate;
-let maxNum = 10;
+let maxNum = 9;
+let minNum = 0;
+let maxRounds = 10;
 
-let randomPlaetzchen = ''
-let randomColor = ''
 // Zufallszahl zwischen 0 und max
 /*function getRandomInt(max) {
     return Math.floor(Math.random() * max);
@@ -22,22 +22,21 @@ function randomIntFromInterval(min, max) {
 function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
   }
-  function getRandomInt(maxNum) {
+function getRandomInt(maxNum) {
     return Math.random() * maxNum;
-  }
+}
+function randomIntFromInterval(minNum, maxNum) {
+    return Math.floor(Math.random() * (maxNum - minNum +1) +minNum)
+}
 
+//Array mit Plätzchen
 const plaetzchen = [{name:'Zimtsterne', farbe:'white'}, {name:'Vanillekipferl', farbe:'brown'}, {name:'Spitzbuben', farbe:'red'}, {name:'Kokosmakronen', farbe:'yellow'}, {name:'Butterplätzchen', farbe:'purple'}, {name:'Schokoladentatzen', farbe: 'blue'}, {name:'Mandelplätzchen', farbe:'green'}, {name: 'Lebkuchen', farbe:'pink'}, {name: 'Engelsaugen', farbe:'beige'}, {name: 'Bethmännchen', farbe:'grey'}]
+//for loop to display all names
+for(let i=0; i< maxRounds; i++) {
 
-let maxRounds = 10;
-    //for loop to display all names
-    for(let i=0; i< maxRounds; i++) {
-
-        plaetzchenNames = plaetzchen[i].name + '' + plaetzchen[i].farbe
-        document.getElementById("menu").innerHTML +=  '<br>' + plaetzchen[i].name + ' with the color ' + plaetzchen[i].farbe
-    }
-
-randomPlaetzchen = plaetzchen[getRandomInt(plaetzchen.length)]
-randomColor = randomPlaetzchen.farbe;
+    plaetzchenNames = plaetzchen[i].name + '' + plaetzchen[i].farbe
+    document.getElementById("menu").innerHTML +=  '<br>' + plaetzchen[i].name + ' with the color ' + plaetzchen[i].farbe
+}
 
 // hier geht das Programm los:
 function setup() {
@@ -51,9 +50,11 @@ function setup() {
 
     //for loop für Plätzchen auf Teller
     for (let j=0; j<8; j++) {
-    fill(plaetzchen[j].farbe);
-    circle(getRandomArbitrary(min, max), getRandomArbitrary(min, max), diameter);
-    document.getElementById("selection").innerHTML +=  plaetzchen[j].name + ' in ' + plaetzchen[j].farbe +', '
+    randomPlaetzchen = plaetzchen[randomIntFromInterval(minNum, maxNum)];
+    randomFarbe = randomPlaetzchen.farbe
+    fill(randomPlaetzchen.farbe);
+    circle(randomIntFromInterval(min, max), randomIntFromInterval(min, max), diameter);
+    document.getElementById("selection").innerHTML +=  randomPlaetzchen.name + ' in ' + randomPlaetzchen.farbe +', '
     }
 }
 
