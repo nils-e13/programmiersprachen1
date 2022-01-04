19ee22e8bc04c35764a20e2d00e9d7370ba8b64e
nils-e13 <nils@sup2o.com>
2022-01-04 23:50:13 +0100

    added working home Button and Page with final score, also added some HTMl Quiz Questions

--- quiz_nils/quizV2.js
+++ quiz_nils/quizV2.js
@@ -49,9 +49,79 @@ let JSquestion5 = {
     ]
 }
 
+let JSquestion6 = {
+    question: 'How do you comment in JavaScript?',
+    answers: 
+    [
+    {choice: '&lt;!--comment--&gt;', correct: false},
+    {choice: '//comment', correct: true},
+    {choice: '#comment', correct: false}
+    ]
+}
+
+let JSquestion7 = {
+    question: 'How do you write an object in JavaScript?',
+    answers: 
+    [
+    {choice: '{let object = brand:"Porsche", name:"Taycan"}', correct: false},
+    {choice: 'const car = {brand:"Porsche",name:"Taycan"}', correct: true},
+    {choice: 'const car = [brand:"Porsche", name:"Taycan"]', correct: false}
+    ]
+}
+
+let HTMLquestion1 = {
+    question: 'Which is the correct HTML element for the largest heading?',
+    answers: 
+    [
+    {choice: '&lt;h6&gt;', correct: false},
+    {choice: '&lt;h1&gt;', correct: true},
+    {choice: '&lt;heading&gt;', correct: false}
+    ]
+}
+
+let HTMLquestion2 = {
+    question: 'What do we write at the beginning of a HTML document?',
+    answers: 
+    [
+    {choice: '&lt;meta charset="utf-8"/&gt;', correct: false},
+    {choice: '&lt;!DOCTYPE html&gt;', correct: true},
+    {choice: '&lt;html&gt;', correct: false}
+    ]
+}
+
+let HTMLquestion3 = {
+    question: 'Which character is used to indicate an end tag?',
+    answers: 
+    [
+    {choice: '&gt;', correct: false},
+    {choice: '/', correct: true},
+    {choice: '&lt;', correct: false}
+    ]
+}
+
+let HTMLquestion4 = {
+    question: 'With which tag does an ordered list start?',
+    answers: 
+    [
+    {choice: '&lt;ul&gt;', correct: false},
+    {choice: '&lt;ol&gt;', correct: true},
+    {choice: '&lt;li&gt;', correct: false}
+    ]
+}
+
+let HTMLquestion5 = {
+    question: 'What is the correct HTML for inserting an image?',
+    answers: 
+    [
+    {choice: '&lt;img href="image.jpg" alt="Image"&gt;', correct: false},
+    {choice: '&lt;img src="image.jpg" alt="Image"&gt;', correct: true},
+    {choice: '&lt;image src="image.jpg" alt="Image"&gt;', correct: false}
+    ]
+}
+
 //Array for set of questions
-const JSFragen = [JSquestion1, JSquestion2, JSquestion3, JSquestion4, JSquestion5];
-const HTMLFragen = []
+const JSFragen = [JSquestion1, JSquestion2, JSquestion3, JSquestion4, JSquestion5, JSquestion6, JSquestion7];
+const HTMLFragen = [HTMLquestion1, HTMLquestion2, HTMLquestion3, HTMLquestion4, HTMLquestion5]
 const CSSFragen = []
 //for multiple Quizes: const JSFragen = ..., const HTMLFragen = ...
 
@@ -134,11 +204,13 @@ function showNextQuestion () {
 
     document.getElementById("home").style.display = 'none';
     document.getElementById("QuestionPage").style.display = 'block';
-    console.log(questionCount);
     }
     else {
+        document.getElementById("scoreFinal").innerHTML = score;
+        //questionCount = 0;
         document.getElementById("QuestionPage").style.display = 'none';
         document.getElementById("EndPage").style.display = 'block';
+        console.log(score);
     }
 }
 
@@ -195,6 +267,10 @@ function proofChoice2() {
 function HomeButton() {
     document.getElementById("home").style.display = 'block';
     document.getElementById("EndPage").style.display = 'none';
+    questionCount = 0;
+    score = 0;
+    document.getElementById("score").innerHTML = score;
+    
 }
 
 //Button to restart current Quiz
